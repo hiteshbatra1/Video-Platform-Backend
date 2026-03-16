e8e1ee85a151fef3a087dc74e3ac8f076a571b5d
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-03-16 15:21:47 +0530

    feat: add standardized ApiError and ApiResponse utility classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import { app } from "./app.js";
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at PORT:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection Failed!", error);
+  });
